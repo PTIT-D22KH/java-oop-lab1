dae14d916567c53644989d2c9b0f5d150b3386df
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-10-06 11:07:51 +0700

    first commit

---
+++ src/java/oop/lab1/JavaOopLab1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java.oop.lab1;
+
+/**
+ *
+ * @author P51
+ */
+public class JavaOopLab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
